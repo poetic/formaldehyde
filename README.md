43ef6583ba248a39f12eeda788968a65914ee7e3
Matthew Hager <matthew@poeticsystems.com>
2015-04-21 17:50:51 -0500

    Update package.js and README.md.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 `meteor add poeticsystems:formaldehyde`
 
-This meteor package allows you to easily manage application state using URL parameters.
+Easily manage application state using URL parameters.
--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'formaldehyde',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  summary: 'Easily manage application state using URL parameters.',
+  git: 'https://github.com/poetic/formaldehyde.git',
   documentation: 'README.md'
 });
 
