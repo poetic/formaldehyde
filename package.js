32e2054a0beef08087fbc188acb772560fa8043c
Leonidez Acosta <leonidez@poeticsystems.com>
2015-05-07 09:38:35 -0500

    version 0.0.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'poeticsystems:formaldehyde',
-  version: '0.0.2',
+  version: '0.0.3',
   summary: 'Easily manage application state using URL parameters.',
   git: 'https://github.com/poetic/formaldehyde.git',
   documentation: 'README.md'
