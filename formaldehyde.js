9dc3bdd7c22bceff7601a585b2d561b1ffc0ee85
John Rodney <haloismyvice@gmail.com>
2015-05-08 00:07:27 -0500

    small restructure public functions at the top, private at the bottom, also refactors RemoveParam to forEach syntax

--- formaldehyde.js
+++ formaldehyde.js
@@ -1,13 +1,16 @@
 if(Meteor.isClient){
+
   // Check if the Meteor.Poetic object is already populated... if we use this as a normal namespace
   // Some other module may have already declared it.. if not then it needs to be instantiated as an empty
   // object
+
   if (typeof Meteor.Poetic === "undefined") {
     Meteor.Poetic = {};
   }
   else{
     console.log("Meteor.Poetic already exhists");
   }
+
   // Register the ParamManager under the Meteor.Poetic namespace
   Meteor.Poetic.ParamManager = (function(){
 
@@ -46,18 +49,68 @@ if(Meteor.isClient){
     }
 
     // This allows you to deregister a param callback by paramname
+
     Interface.DeRegisterParam = function(paramName){
       // iterate through the params object and find a match to the paramName passed then remove it
-      for(var i = 0; i < Params.length; i++){
-        if(Params[i].param === paramName){
-          Params.splice(i, 1);
-          i = Params.length;
+
+      Params.forEach(function(param, i, params){
+        if(param.param === paramName){
+          params.splice(i, 1);
         }
-      }
+      });
     }
 
     Interface.isRegistered = function(paramName){
       return Params.some(function(param){ return param.param === paramName });
+    }
+
+
+    // this function needs to push the state of the window, build a new URL based on values passed and then trigger any
+    // callback functions that are rigistered to the change in url replace is a third optional arguement to update params
+    // but not push the state forward.
+
+    Interface.setParam = function(param, value, replace){
+      buildUrl(param, value, replace);                              // pass the values to build a new url
+      document.dispatchEvent(ChangedURL);                 // url has changed fire an event to trigger callbacks
+    }
+
+    // allow the user an easy interface to find or poll for the value of param. This shouldn't ever be needed if the que
+    // Set and Link methods are used properly, but it does allow for restful principles and easier debugging.
+
+    Interface.getParam = function(param){
+      return getParameterByName(param);
+    }
+
+    // ChangedURL is the event that will be listened to for url changes. Because of this you should always use
+    // the paramManager to update the parameter url or ELSE your function will NOT be called.
+
+    var ChangedURL = new Event('urlchange');
+
+    // when the document is ready register our callbacks function on the urlchange event.
+    window.onload = function(){
+      document.addEventListener('urlchange', function(event){
+        executeCallbacks();
+      }, false);
+      // register forward backwards and directly typed urls to fire this event.
+      window.onpopstate = function(){
+        document.dispatchEvent(ChangedURL);
+      }
+      document.dispatchEvent(ChangedURL);
+    };
+
+
+    // executeCallbacks will iterate through every object in the params array and execute its callback.
+    // It will check its current value and the value in the URL if these values are the same then its callback
+    // won't be called. If the values are different then the url was Changed so then it will run its callback and then
+    // update its internal value to the new value
+    function executeCallbacks(){
+      Params.forEach(function(curParam, i){
+        var curVal = getParameterByName(curParam.param);
+        if(curParam.value !== curVal){
+          curParam.callback(curVal);
+          curParam.value = curVal;
+        }
+      });
     }
 
     // build URL is the interface set to trigger any callbacks whose state may have changed from the current set call
@@ -89,58 +142,14 @@ if(Meteor.isClient){
       }
     }
 
-    // this function needs to push the state of the window, build a new URL based on values passed and then trigger any
-    // callback functions that are rigistered to the change in url replace is a third optional arguement to update params
-    // but not push the state forward.
-    Interface.setParam = function(param, value, replace){
-      buildUrl(param, value, replace);                              // pass the values to build a new url
-      document.dispatchEvent(ChangedURL);                 // url has changed fire an event to trigger callbacks
-    }
-
-    // allow the user an easy interface to find or poll for the value of param. This shouldn't ever be needed if the que
-    // Set and Link methods are used properly, but it does allow for restful principles and easier debugging.
-    Interface.getParam = function(param){
-      return getParameterByName(param);
-    }
-
-    // ChangedURL is the event that will be listened to for url changes. Because of this you should always use
-    // the paramManager to update the parameter url or ELSE your function will NOT be called.
-
-    var ChangedURL = new Event('urlchange');
-
-    // executeCallbacks will iterate through every object in the params array and execute its callback.
-    // It will check its current value and the value in the URL if these values are the same then its callback
-    // won't be called. If the values are different then the url was Changed so then it will run its callback and then
-    // update its internal value to the new value
-    function executeCallbacks(){
-      Params.forEach(function(curParam, i){
-        var curVal = getParameterByName(curParam.param);
-        if(curParam.value !== curVal){
-          curParam.callback(curVal);
-          curParam.value = curVal;
-        }
-      });
-    }
-
-    // when the document is ready register our callbacks function on the urlchange event.
-    window.onload = function(){
-      document.addEventListener('urlchange', function(event){
-        executeCallbacks();
-      }, false);
-      // register forward backwards and directly typed urls to fire this event.
-      window.onpopstate = function(){
-        document.dispatchEvent(ChangedURL);
-      }
-      document.dispatchEvent(ChangedURL);
-    };
-
-    // simple function that returns the value of a query param by name from the current url.
+    // function that returns the value of a query param by name from the current url.
     function getParameterByName(name) {
         name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
         var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
             results = regex.exec(location.search);
         return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
     }
+
     return Interface;
   })();
 }
