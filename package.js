9540309b7a4208a15f33bcd792511487dedcd086
Leonidez Acosta <leonidez@poeticsystems.com>
2015-09-20 18:10:04 -0500

    update babel dependency

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'poetic:formaldehyde',
-  version: '0.0.7',
+  version: '0.0.8',
   summary: 'Easily manage application state using URL parameters.',
   git: 'https://github.com/poetic/formaldehyde.git',
   documentation: 'README.md'
@@ -8,7 +8,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.use('grigio:babel@0.1.4');
+  api.use('grigio:babel@0.1.8');
   api.addFiles('formaldehyde.es6.js');
 });
 
