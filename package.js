a0fbcab448c9e2cc5f8fc022804d0c98f14e0d2a
Leonidez Acosta <leonidez@poeticsystems.com>
2015-10-13 17:43:58 -0500

    bump version and update package.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'poetic:formaldehyde',
-  version: '0.1.1',
+  version: '0.1.2',
   summary: 'Easily manage application state using URL parameters.',
   git: 'https://github.com/poetic/formaldehyde.git',
   documentation: 'README.md'
@@ -9,11 +9,12 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
 
-  api.use('meteor-platform@1.2.2')
   api.use('check@1.0.5');
   api.use('grigio:babel@0.1.8');
 
-  api.addFiles('formaldehyde.es6.js');
+  api.addFiles('formaldehyde.es6.js', 'client');
+
+  api.export('Formaldehyde', 'client');
 });
 
 Package.onTest(function(api) {
